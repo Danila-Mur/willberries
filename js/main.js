56688b90fece97f8675a9dd4accefebbbd562ce8
Danila-Mur <66494739+Danila-Mur@users.noreply.github.com>
2021-01-30 01:35:15 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.swiper-container', {
+    loop: true,
+  // Navigation arrows
+    navigation: {
+        nextEl: '.slide-button-next',
+        prevEl: '.slider-button-prev',
+},
+
+});
